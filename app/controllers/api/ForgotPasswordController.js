f30eb0b1ec816d746bfb78653ee89421f703c290
Huda Prasetyo <huda.prs2002@gmail.com>
2020-07-15 18:51:19 +0700

    completing forgot password API

--- app/controllers/api/ForgotPasswordController.js
+++ app/controllers/api/ForgotPasswordController.js
@@ -1,5 +1,6 @@
 const { success, error, validation } = require("../../helpers/responseApi");
 const { randomString } = require("../../helpers/common");
+const bcrypt = require("bcryptjs");
 const User = require("../../models/User");
 const Verification = require("../../models/Verification");
 
@@ -55,6 +56,68 @@ exports.forgot = async (req, res) => {
           res.statusCode
         )
       );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json(error("Server error", res.statusCode));
+  }
+};
+
+/**
+ * @desc    Resetting user password
+ * @method  POST api/password/reset
+ * @access  public
+ */
+exports.reset = async (req, res) => {
+  const { token, password } = req.body;
+
+  // Check the token first
+  if (!token)
+    return res.status(422).json(validation([{ msg: "Token is required" }]));
+
+  // Check the password
+  if (!password)
+    return res.status(422).json(validation([{ msg: "Password is required" }]));
+
+  try {
+    let verification = await Verification.findOne({
+      token,
+      type: "Forgot Password"
+    });
+
+    // Check the verification data
+    if (!verification)
+      return res
+        .status(400)
+        .json(
+          error("Token / Data that you input is not valid", res.statusCode)
+        );
+
+    // If there's verification data
+    // Let's find the user first
+    let user = await User.findById(verification.userId);
+
+    // Check the user, just in case
+    if (!user)
+      return res.status(404).json(error("User not found", res.statusCode));
+
+    // if those condition all passed
+    // Let's update the password
+    // Dont forget to hash the password using bcrypt
+    let hash = await bcrypt.genSalt(10);
+    let hashedPassword = await bcrypt.hash(password, hash);
+
+    // Finnaly, update the user password
+    user = await User.findByIdAndUpdate(user._id, {
+      password: hashedPassword
+    });
+
+    // Lets delete the verification data
+    verification = await Verification.findByIdAndRemove(verification._id);
+
+    // Send the response
+    res
+      .status(200)
+      .json(success("Password has been updated", null, res.statusCode));
   } catch (err) {
     console.error(err.message);
     res.status(500).json(error("Server error", res.statusCode));
--- routes/api/forgotPassword.js
+++ routes/api/forgotPassword.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 
 // Controllers
 const {
-  forgot
+  forgot,
+  reset
 } = require("../../app/controllers/api/ForgotPasswordController");
 
 // Routes
 router.post("/forgot", forgot);
+router.post("/reset", reset);
 
 module.exports = router;
